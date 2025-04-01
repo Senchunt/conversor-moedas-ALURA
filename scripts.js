e7cb087794ba1dcdb6347f9c4f8692e4e397050b
BRUNO FERREIRA de ARAÚJO <71849959+Senchunt@users.noreply.github.com>
2025-04-01 08:15:16 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+valordollar = prompt("Digite o valor em REAIS para obter a equivalência em DOLAR!")
+umdollar = 5.71
+alert("o valor é " + valordollar * umdollar + " dolares!")
+
+valoreuros = prompt("Digite o valor em REAIS para obter a equivalência em EUROS!")
+umeuro = 6.17
+alert("o valor é " + valoreuros * umeuro + " euros!")
